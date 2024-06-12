b708290ea120f008e906db33e6d612bfc846c49f
Ehab Alaama <ehab.alaama@socar.my>
2024-06-13 02:45:10 +0800

    refactored the consumer to use streams for proccessing the input and output files instead of fs methods

--- consumer.js
+++ consumer.js
@@ -1,35 +1,45 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
+const readline = require("readline");
 
-const inputFilePath = path.join(__dirname, 'output', 'output.txt');
-const outputFilePath = path.join(__dirname, 'output', 'processed_output.json');
+const inputFilePath = path.join(__dirname, "output", "output.txt");
+const outputFilePath = path.join(__dirname, "output", "processed_output.json");
 
 const processFile = () => {
-    fs.readFile(inputFilePath, 'utf8', (err, data) => {
-        if (err) throw err;
+  const readStream = fs.createReadStream(inputFilePath, "utf8");
+  const writeStream = fs.createWriteStream(outputFilePath);
+  const rl = readline.createInterface({
+    input: readStream,
+    terminal: false,
+  });
 
-        const objects = data.split(',')
-            .map(item => item.trim()) // Removes spaces from any string
-            .filter(item => item)
-            .map(value => {
-                let type;
-                if (/^\d+$/.test(value)) {
-                    type = 'Integer';
-                } else if (/^\d+\.\d+$/.test(value)) {
-                    type = 'Real Number';
-                } else if (/\d/.test(value)) {
-                    type = 'Alphanumeric';
-                } else {
-                    type = 'Alphabetical String';
-                }
-                return { value, type };
-            });
+  const objects = [];
 
-        fs.writeFile(outputFilePath, JSON.stringify(objects, null, 2), err => {
-            if (err) throw err;
-            console.log('Finished writing processed_output.json');
-        });
+  rl.on("line", (line) => { // read the output.txt file line by line
+    const items = line
+      .split(",")
+      .map((item) => item.trim()) // Removes spaces from strings
+      .filter((item) => item); // Removes any empty strings
+    items.forEach((value) => {
+      let type;
+      if (/^\d+$/.test(value)) {
+        type = "Integer";
+      } else if (/^\d+\.\d+$/.test(value)) {
+        type = "Real Number";
+      } else if (/\d/.test(value)) {
+        type = "Alphanumeric";
+      } else {
+        type = "Alphabetical String";
+      }
+      objects.push({ value, type });
     });
+  });
+
+  rl.on("close", () => {
+    writeStream.write(JSON.stringify(objects, null, 2));
+    writeStream.end();
+    console.log("Finished writing processed_output.json");
+  });
 };
 
 processFile();
